96f45d130f40eabb5fbc5220762421d17fe2ba3a
zeia22 <122165265+zeia22@users.noreply.github.com>
2023-01-09 14:27:47 -0300

    Criando a entrada de tarefas

--- todo4/src/App.js
+++ todo4/src/App.js
@@ -1,9 +1,54 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import "./index.css";
 
-const App = () => (
-  <h1 className='title'>App</h1>
+const App = () => {
+  const ESCAPE_KEY = 27;
+  const ENTER_KEY = 13;
+
+  const [value, setValue] = useState('');
+
+  const erase = () => {
+    setValue('');
+  }
+  const submit = () => {
+    console.log('submit', value);
+      erase();
+  }
+
+  const onChange =(event) => {
+    setValue(event.target.value);
+  }
+
+  const onKeyDow = (event)  => {
+    if (event.which === ENTER_KEY) {
+      submit();
+    }else if (event.wich === ESCAPE_KEY)  {
+      erase();
+    }
+  }
+
+  return(
+  <section>
+    <header>
+  <h1 className='title'>Todo</h1>
+    </header>
+
+
+
+
+   <section className='main'>
+    <input
+    className="new-todo"
+    placeholder="o que precisa ser feito?"
+    value={value}
+    onChange={onChange}
+    onkeyDow={onkeyDow}
+    />
+    </section>
+   </section>
   );
+}
 
 
 export default App;
